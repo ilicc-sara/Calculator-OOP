0a92cb04bf226b9c1f13f632ace16a839671dc70
Sara <sarailic431@gmail.com>
2025-02-24 16:03:34 +0100

    starting JS on calculator

--- src/main.js
+++ src/main.js
@@ -1,2 +1,16 @@
 "use strict";
 import "./style.css";
+
+const firstNumberEl = document.querySelector(".first");
+const secondNumberEl = document.querySelector(".second");
+
+const clearBtn = document.querySelector(".clear");
+const deleteBtn = document.querySelector(".delete");
+
+class Calculator {
+  constructor(firstNumber, secondNumber, operation) {
+    this.firstNumber = firstNumber;
+    this.secondNumber = secondNumber;
+    this.operation = operation;
+  }
+}
