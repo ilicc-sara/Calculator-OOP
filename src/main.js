b65c2c8bf47cb19f7536947a900c8ac2d6096a88
Sara <sarailic431@gmail.com>
2025-02-24 20:20:49 +0100

    ...

--- src/main.js
+++ src/main.js
@@ -9,9 +9,29 @@ const deleteBtn = document.querySelector(".delete");
 
 const operationsCont = document.querySelector(".operations");
 
+let firstNumber = "";
+let secondNumber = "";
+let operation = "";
+
 operationsCont.addEventListener("click", function (e) {
   // prettier-ignore
   if (!e.target.classList.contains('num-btn') && !e.target.classList.contains('op-btn') && !e.target.classList.contains('equals')) return;
+
+  if (e.target.classList.contains("num-btn")) {
+    firstNumber = e.target.getAttribute("data-id");
+    console.log(firstNumber);
+  }
+
+  if (e.target.classList.contains("op-btn")) {
+    operation = e.target.getAttribute("data-id");
+    console.log(operation);
+  }
+
+  if (firstNumber !== "") {
+    secondNumber = firstNumber;
+    console.log(secondNumber);
+    firstNumber = "";
+  }
 });
 
 class Calculator {
